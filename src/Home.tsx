fbbbd955bc88272282ad9635358cf27a8e8f176a
Hsun <hsun1031@outlook.com>
2023-05-12 14:07:08 +0000

    ✨ feat: Add Home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,70 @@
+import axios from 'axios';
+
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { CaretUpFill, CaretRightFill, CaretLeftFill, CaretDownFill } from 'react-bootstrap-icons';
+
 const Home = () => {
+    /**
+     * Send command to ESP32
+     * @param command 0: stop, 1: forward, 2: backward, 3: right, 4: left
+     */
+    const sendCommand = (command: number) => {
+        console.log(`sendCommand[${command}]`);
+
+        // POST /command
+        // axios.post('/command', { command: command });
+    }
+
     return (
-        <>
-            <h1>Hello Home!</h1>
-        </>
+        <Container className='container'>
+            <Row className='row my-5'></Row>
+            <Row className='row my-3 '>
+                <Col className='col d-grid gap-2'>
+                    <Button className='btn btn-lg btn-outline-info' 
+                            style={{ height: '130px' }}
+                            onMouseDown ={ () => { sendCommand(1); } } 
+                            onMouseUp   ={ () => { sendCommand(0); } }
+                            onTouchStart={ () => { sendCommand(1); } } 
+                            onTouchEnd  ={ () => { sendCommand(0); } } >
+                        <CaretUpFill size={100}/>
+                    </Button>
+                </Col>
+            </Row>
+            <Row className='row my-3'>
+                <Col className='col d-grid gap-2'>
+                    <Button className='btn btn-lg btn-outline-info' 
+                                style={{ height: '200px' }}
+                                onMouseDown ={ () => { sendCommand(4); } } 
+                                onMouseUp   ={ () => { sendCommand(0); } }
+                                onTouchStart={ () => { sendCommand(4); } } 
+                                onTouchEnd  ={ () => { sendCommand(0); } } >
+                        <CaretLeftFill size={100}/>
+                    </Button>
+                </Col>
+                <Col className='col d-grid gap-2'>
+                    <Button className='btn btn-lg btn-outline-info' 
+                                style={{ height: '200px' }}
+                                onMouseDown ={ () => { sendCommand(3); } } 
+                                onMouseUp   ={ () => { sendCommand(0); } }
+                                onTouchStart={ () => { sendCommand(3); } } 
+                                onTouchEnd  ={ () => { sendCommand(0); } } >
+                        <CaretRightFill size={100}/>
+                    </Button>
+                </Col>
+            </Row>
+            <Row className='row my-3'>
+                <Col className='col d-grid gap-2'>
+                <Button className='btn btn-lg btn-outline-info' 
+                            style={{ height: '130px' }}
+                            onMouseDown ={ () => { sendCommand(2); } } 
+                            onMouseUp   ={ () => { sendCommand(0); } }
+                            onTouchStart={ () => { sendCommand(2); } } 
+                            onTouchEnd  ={ () => { sendCommand(0); } } >
+                        <CaretDownFill size={100}/>
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
